da30bc9ece13b46a5bd58ae3975b09f68ec8462c
Vlad <serveladik@gmail.com>
2020-02-10 16:51:39 +0200

    SplashTurret

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     private Transform enemy;
     public float speed=80f;
+    public float splashRange = 0f;
     public GameObject hitParticles;
     public void Seeking(Transform _enemy)
     {
@@ -27,14 +28,44 @@ public class Bullet : MonoBehaviour
             return;
         }
         transform.Translate(direction.normalized * distance, Space.World);
+        //transform.LookAt(enemy.position);
     }
     void HitEnemy()
     {
         GameObject effect = (GameObject)Instantiate(hitParticles,transform.position,transform.rotation);
         Destroy(effect,2f);
-        Destroy(gameObject);
+        if(splashRange>0f)
+        {
+            Explode();
+        }
+        else
+        {
+            SplashDamage(enemy);
+        }
         Destroy(enemy.gameObject);
+        Destroy(gameObject);
         
+        
+    }
+    void Explode()
+    {
+        Collider[] enemiesInSplash = Physics.OverlapSphere(transform.position,splashRange);
+        foreach(Collider splashTragets in enemiesInSplash)
+        {
+            if(splashTragets.tag =="Enemy")
+            {
+                SplashDamage(splashTragets.transform);
+            }
+        }
+    }
+    void SplashDamage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,splashRange);
     }
     void Update()
     {
