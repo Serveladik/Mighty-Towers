05d215cdd876f309da57c611b4a02c28b1abfa03
Vlad <serveladik@gmail.com>
2020-03-05 12:43:04 +0200

    as

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject pauseUI;
+    void Update()
+    {
+        
+    }
+    public void Pause()
+    {
+        pauseUI.SetActive(!pauseUI.activeSelf);
+        if(pauseUI.activeSelf)
+        {
+            Time.timeScale=0f;
+        }
+        else
+        {
+            Time.timeScale=1f;
+        }
+    }
+    public void Settings()
+    {
+        //settingsUI.SetActive(!settingsUI.activeSelf);
+    }
+
+}
