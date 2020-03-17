4a82306e9e76fd4f5e84df58f7c2ead6286bd752
Vlad <serveladik@gmail.com>
2020-03-17 16:44:22 +0200

    MainMenu(Select level + animations)

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public Animator animation;
+    
+   public void SelectScene(string level)
+   {
+        StartCoroutine(LoadScene(level));
+   }
+
+
+    public IEnumerator LoadScene(string level)
+    {
+        animation.SetBool("LoadingLevel",true);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(level);
+    }
+
+}
--- Assets/MenuPlayButton.cs
+++ Assets/MenuPlayButton.cs
@@ -5,7 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MenuPlayButton : MonoBehaviour
 {
+    public GameObject SelectLevel;
+    public GameObject mainMenu;
     public Animator menuAnim;
+    public Animator levelSelectAnim;
     void Awake()
     {
         Time.timeScale=1f;
@@ -14,10 +17,23 @@ public class MenuPlayButton : MonoBehaviour
     {
         StartCoroutine("PlayButton");
     }
+    public void BackBtn()
+    {
+        StartCoroutine("BackButton");
+    }
    public IEnumerator PlayButton()
    {
        menuAnim.SetBool("Play",true);
-       yield return new WaitForSeconds(2f);
-       SceneManager.LoadScene("Level1");
+       yield return new WaitForSeconds(1f);
+       mainMenu.SetActive(false);
+       SelectLevel.SetActive(true);
+   }
+   public IEnumerator BackButton()
+   {
+       menuAnim.SetBool("Play",false);
+       levelSelectAnim.SetBool("LoadingLevel",true);
+       yield return new WaitForSeconds(0.5f);
+       mainMenu.SetActive(true);
+       SelectLevel.SetActive(false);
    }
 }
